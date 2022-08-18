b422a308f0b3438656e4fb71424aa0279b41025e
Akeiba <Akeiba@niyoenterprise.com>
2022-08-18 10:20:23 +0100

    API consumption practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import User from "./components/User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <p>Akeiba's API Consumption Practice</p>
+        </header>
+
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/user/:id" element={<User />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/User.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const User = (props) => {
+  const { id } = useParams();
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [user, setUser] = useState([]);
+  const [userAddress, setUserAddress] = useState([]);
+  const [userCompany, setUserCompany] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/" + id)
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          console.log(data);
+          setUser(data);
+          setIsLoaded(true);
+          setUserAddress(data.address);
+          setUserCompany(data.company);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+  if (user) {
+    return (
+      <>
+        <h1>{user.name}</h1>
+        <div>Email: {user.email}</div>
+        <div>Phone: {user.phone}</div>
+        <div>Website: {user.website}</div>
+        <div>Company: {userCompany.name}</div>
+        <div>
+          Address: {userAddress.suite}, {userAddress.street}, {userAddress.city}
+          , {userAddress.zipcode}
+        </div>
+      </>
+    );
+  }
+};
+
+export default User;
